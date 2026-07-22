fa287e81de9a25cac493ecf0825a5da988cb9fd3
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-22 06:15:56 -0700

    Update monitoring-iam.tf

--- terraform/monitoring-iam.tf
+++ terraform/monitoring-iam.tf
@@ -64,3 +64,40 @@ resource "aws_iam_instance_profile" "monitoring" {
   role = aws_iam_role.monitoring.name
 
 }
+
+# ==========================================================
+# ======================================================
+
+###############################################################
+# Prometheus EC2 Service Discovery Policy
+###############################################################
+
+resource "aws_iam_policy" "prometheus_ec2_discovery" {
+  name        = "prometheus-ec2-discovery-policy"
+  description = "Allows Prometheus to discover backend EC2 instances"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeAvailabilityZones"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "prometheus_ec2_discovery" {
+
+  role       = aws_iam_role.monitoring.name
+  policy_arn = aws_iam_policy.prometheus_ec2_discovery.arn
+
+}
