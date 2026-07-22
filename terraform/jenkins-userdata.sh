36d00e78718bd386383e0f37df6f7702fabb883e
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-22 09:55:23 -0700

    Update jenkins-userdata.sh

--- terraform/jenkins-userdata.sh
+++ terraform/jenkins-userdata.sh
@@ -163,6 +163,11 @@ ENV PATH="$PATH:/opt/dependency-check/bin"
 
 RUN usermod -aG docker jenkins
 
+COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
+
+RUN jenkins-plugin-cli \
+    --plugin-file /usr/share/jenkins/ref/plugins.txt
+
 USER jenkins
 EOF
 
