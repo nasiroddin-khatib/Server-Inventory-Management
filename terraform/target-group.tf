62d99a69bf4cecb425b55dedc82ccb217179ec79
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-22 00:41:42 -0700

    Update target-group.tf

--- terraform/target-group.tf
+++ terraform/target-group.tf
@@ -1,1 +1,42 @@
+############################################
+# Backend Target Group
+############################################
 
+resource "aws_lb_target_group" "backend" {
+
+  name        = "backend-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "instance"
+
+  vpc_id = aws_vpc.vpc.id
+
+  health_check {
+
+    enabled = true
+
+    protocol = "HTTP"
+
+    path = "/server-inventory/actuator/health"
+
+    port = "traffic-port"
+
+    matcher = "200"
+
+    interval = 30
+
+    timeout = 5
+
+    healthy_threshold = 2
+
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "backend-target-group"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+}
