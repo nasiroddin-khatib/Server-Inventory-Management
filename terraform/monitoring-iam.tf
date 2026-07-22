0aa7192195e29e7b7adb9dfed2b9980af5298cc9
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-22 03:47:51 -0700

    Create monitoring-iam.tf

---
+++ terraform/monitoring-iam.tf
@@ -0,0 +1,66 @@
+############################################
+# Monitoring EC2 IAM Role
+############################################
+
+resource "aws_iam_role" "monitoring" {
+
+  name = "monitoring-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "monitoring-ec2-role"
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+}
+
+############################################
+# CloudWatch Agent Policy
+############################################
+
+resource "aws_iam_role_policy_attachment" "monitoring_cloudwatch" {
+
+  role       = aws_iam_role.monitoring.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+
+}
+
+############################################
+# SSM Policy
+############################################
+
+resource "aws_iam_role_policy_attachment" "monitoring_ssm" {
+
+  role       = aws_iam_role.monitoring.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+
+}
+
+############################################
+# Instance Profile
+############################################
+
+resource "aws_iam_instance_profile" "monitoring" {
+
+  name = "monitoring-instance-profile"
+
+  role = aws_iam_role.monitoring.name
+
+}
