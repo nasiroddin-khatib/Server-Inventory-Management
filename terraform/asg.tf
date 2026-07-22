d25f0f30a9aea2f6dd418a0d43075b6d7e18b229
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-22 01:39:28 -0700

    Create asg.tf

---
+++ terraform/asg.tf
@@ -0,0 +1,86 @@
+############################################
+# Backend Auto Scaling Group
+############################################
+
+resource "aws_autoscaling_group" "backend" {
+
+  name = "backend-asg"
+
+  min_size         = 2
+  desired_capacity = 2
+  max_size         = 4
+
+  vpc_zone_identifier = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  target_group_arns = [
+    aws_lb_target_group.backend.arn
+  ]
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    id      = aws_launch_template.backend.id
+    version = "$Latest"
+  }
+
+instance_refresh {
+
+  strategy = "Rolling"
+
+  preferences {
+
+    min_healthy_percentage = 50
+
+    instance_warmup = 300
+
+  }
+
+  triggers = [
+    "launch_template"
+  ]
+
+}
+
+  termination_policies = [
+    "OldestLaunchTemplate"
+  ]
+
+  enabled_metrics = [
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupTotalInstances"
+  ]
+
+  tag {
+    key                 = "Name"
+    value               = "backend-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Project"
+    value               = var.project
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Environment"
+    value               = var.environment
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "ManagedBy"
+    value               = "Terraform"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
