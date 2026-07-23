2d67fb6e7fe66f10ffc6fd13b51f1d7a30423265
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-23 05:41:39 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -269,3 +269,15 @@ output "scale_out_policy_arn" {
 output "scale_in_policy_arn" {
   value = aws_autoscaling_policy.scale_in.arn
 }
+
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.frontend.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
