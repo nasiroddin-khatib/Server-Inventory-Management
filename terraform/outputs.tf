7860ee36c2a2301a1af016965967843c7b52a96f
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-07-25 12:15:21 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -274,6 +274,14 @@ output "frontend_bucket_name" {
   value = aws_s3_bucket.frontend.bucket
 }
 
+### output "cloudfront_domain_name" {
+###  value = aws_cloudfront_distribution.frontend.domain_name
+### }
+
+#### output "cloudfront_distribution_id" {
+####  value = aws_cloudfront_distribution.frontend.id
+#### }
+
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.frontend.domain_name
 }
