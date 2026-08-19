1d1545d678177c75a6d41134b7633a67290eb571
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-19 12:59:21 -0700

    Update secrets-manager-sonarqube.tf

--- terraform/secrets-manager-sonarqube.tf
+++ terraform/secrets-manager-sonarqube.tf
@@ -1,14 +1,18 @@
 # ============================================================
 # SonarQube Database Secret
 # ============================================================
+#resource "aws_secretsmanager_secret" "sonarqube_database" {
+# name        = "server-inventory/sonarqube/database"
+#  description = "SonarQube PostgreSQL database credentials"
+#  tags = {
+#   Name      = "server-inventory-sonarqube-database"
+#    Project   = "Server-Inventory"
+#    ManagedBy = "Terraform"
+# }
+# }
 
-resource "aws_secretsmanager_secret" "sonarqube_database" {
-  name        = "server-inventory/sonarqube/database"
-  description = "SonarQube PostgreSQL database credentials"
 
-  tags = {
-    Name      = "server-inventory-sonarqube-database"
-    Project   = "Server-Inventory"
-    ManagedBy = "Terraform"
-  }
+data "aws_secretsmanager_secret" "sonarqube_database" {
+  name = "server-inventory/sonarqube/database"
 }
+
