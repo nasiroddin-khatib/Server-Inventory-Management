80565730941d921332adb5d896e90d30a133f730
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-21 02:50:04 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -276,3 +276,51 @@ output "packer_security_group_id" {
 
   value = aws_security_group.packer_sg.id
 }
+
+############################################
+# Monitoring Server
+############################################
+
+output "monitoring_instance_id" {
+
+  description = "Monitoring server instance ID"
+
+  value = aws_instance.monitoring.id
+
+}
+
+
+output "monitoring_public_ip" {
+
+  description = "Monitoring server public IP"
+
+  value = aws_instance.monitoring.public_ip
+
+}
+
+
+output "monitoring_public_dns" {
+
+  description = "Monitoring server public DNS"
+
+  value = aws_instance.monitoring.public_dns
+
+}
+
+
+output "prometheus_url" {
+
+  description = "Prometheus web UI"
+
+  value = "http://${aws_instance.monitoring.public_ip}:9090"
+
+}
+
+
+output "grafana_url" {
+
+  description = "Grafana web UI"
+
+  value = "http://${aws_instance.monitoring.public_ip}:3000"
+
+}
