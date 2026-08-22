075546ea0195abb0f2c1ab0fc699ebf6cd75d5e1
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-22 01:00:21 -0700

    Update asg.tf

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "backend" {
   ]
 
   health_check_type         = "ELB"
-  health_check_grace_period = 900
+  health_check_grace_period = 300
 
   launch_template {
     id      = aws_launch_template.backend.id
