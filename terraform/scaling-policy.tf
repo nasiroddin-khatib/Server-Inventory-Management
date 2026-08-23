9ddf22db5812ba2addab841b0b4e17bbf45b6670
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-23 10:41:42 -0700

    Update scaling-policy.tf

--- terraform/scaling-policy.tf
+++ terraform/scaling-policy.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "aws_autoscaling_policy" "scale_out" {
-
+count = var.backend_ami_id != null ? 1 : 0
   name                   = "backend-scale-out"
   autoscaling_group_name = aws_autoscaling_group.backend[0].name
 
@@ -20,7 +20,7 @@ resource "aws_autoscaling_policy" "scale_out" {
 ############################################
 
 resource "aws_autoscaling_policy" "scale_in" {
-
+count = var.backend_ami_id != null ? 1 : 0
   name                   = "backend-scale-in"
   autoscaling_group_name = aws_autoscaling_group.backend[0].name
 
