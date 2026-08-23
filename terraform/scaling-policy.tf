296c2142c2f864c25ea70ab6efb169a4ee44f769
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-23 10:26:10 -0700

    Update scaling-policy.tf

--- terraform/scaling-policy.tf
+++ terraform/scaling-policy.tf
@@ -5,7 +5,7 @@
 resource "aws_autoscaling_policy" "scale_out" {
 
   name                   = "backend-scale-out"
-  autoscaling_group_name = aws_autoscaling_group.backend.name
+  autoscaling_group_name = aws_autoscaling_group.backend[0].name
 
   adjustment_type = "ChangeInCapacity"
 
@@ -22,7 +22,7 @@ resource "aws_autoscaling_policy" "scale_out" {
 resource "aws_autoscaling_policy" "scale_in" {
 
   name                   = "backend-scale-in"
-  autoscaling_group_name = aws_autoscaling_group.backend.name
+  autoscaling_group_name = aws_autoscaling_group.backend[0].name
 
   adjustment_type = "ChangeInCapacity"
 
