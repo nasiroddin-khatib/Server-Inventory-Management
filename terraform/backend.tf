bcd31fb529359e67f9d8b0d1eee564b908d22f9e
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-23 03:48:34 -0700

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-575458732395-ap-south-1-an"
+    key    = "server-inventory/terraform.tfstate"
+    region = "ap-south-1"
+
+    encrypt     = true
+    use_lockfile = true
+  }
+}
