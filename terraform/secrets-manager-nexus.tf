55c840b8d28caeb95a07f9ecd74a8ca8bedcd568
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-08-31 06:28:46 -0700

    Update secrets-manager-nexus.tf

--- terraform/secrets-manager-nexus.tf
+++ terraform/secrets-manager-nexus.tf
@@ -2,15 +2,20 @@
 # Nexus Credentials Secret
 ############################################
 
-resource "aws_secretsmanager_secret" "nexus_credentials" {
+# resource "aws_secretsmanager_secret" "nexus_credentials" {
+#
+#  name        = var.nexus_secret_name
+#  description = "Credentials used by Jenkins to deploy artifacts to Nexus"
+#
+#  tags = {
+#    Name        = var.nexus_secret_name
+#    Project     = var.project
+#    Environment = var.environment
+#    ManagedBy   = "Terraform"
+#  }
+# }
 
-  name        = var.nexus_secret_name
-  description = "Credentials used by Jenkins to deploy artifacts to Nexus"
 
-  tags = {
-    Name        = var.nexus_secret_name
-    Project     = var.project
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
+data "aws_secretsmanager_secret" "nexus_credentials" {
+  name = var.nexus_secret_name
 }
